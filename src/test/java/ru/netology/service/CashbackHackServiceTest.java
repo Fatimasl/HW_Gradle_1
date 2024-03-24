2680812996522abb607c2ed4292b1ab021bb32cc
Neto test <s.e.lebedeva74@gmail.com>
2024-03-24 14:04:13 +0500

    Task done with junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,11 +1,10 @@
 package ru.netology.service;
 
-import static org.testng.Assert.assertEquals;
-
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn450ThenAmount2550(){
 
         CashbackHackService service = new CashbackHackService();
@@ -18,7 +17,7 @@ public class CashbackHackServiceTest {
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn1000ThenAmount5000(){
 
         CashbackHackService service = new CashbackHackService();
@@ -31,7 +30,7 @@ public class CashbackHackServiceTest {
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn1ThenAmount4999(){
 
         CashbackHackService service = new CashbackHackService();
@@ -44,7 +43,7 @@ public class CashbackHackServiceTest {
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn999ThenAmount4001(){
 
         CashbackHackService service = new CashbackHackService();
@@ -52,6 +51,19 @@ public class CashbackHackServiceTest {
         int amount = 4001;
         int actual = service.remain(amount);
         int expected = 999;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @org.junit.Test
+    public void shouldReturn1000ThenAmount0(){
+
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
 
         assertEquals(actual, expected);
 
